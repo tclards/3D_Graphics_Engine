b836e1aaeed663580809dbe3628c6d29fff1d51a
Tyler Clardy <tclards@gmail.com>
2022-07-01 12:05:47 -0500

    Finished basic set up for olcConsoleGameEngine. Ready to start work.

--- 3D_Graphics_Engine/3D_Graphics_Engine.cpp
+++ 3D_Graphics_Engine/3D_Graphics_Engine.cpp
@@ -1,6 +1,35 @@
-#include <iostream>
+#include "olcConsoleGameEngine.h"
+using namespace std;
+
+class olcEngine3D : public olcConsoleGameEngine
+{
+public:
+	olcEngine3D()
+	{
+		m_sAppName = L"3D_Graphics_Engine";
+	}
+
+private:
+
+public:
+	bool OnUserCreate() override
+	{
+
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+
+		return true;
+	}
+};
 
 int main()
 {
-    
+	olcEngine3D graphicsEngine;
+	if (graphicsEngine.ConstructConsole(256, 240, 4, 4))
+	{
+		graphicsEngine.Start();
+	}
+	return 0;
 }
